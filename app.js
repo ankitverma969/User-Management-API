0833a497561488bf56ce64148d4e21484a67b72b
ankitverma969 <blogankitverma@gmail.com>
2026-02-04 14:18:35 +0530

    app.js created

---
+++ app.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import userRoute from "./routes/user.routes.js";
+
+const app  = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("User Management API is running");
+});
+
+// app.use("/api/users", userRoute);
+
+export default app;
